b47b65dd18b26d3a28859353567578dccdd0e8e1
Julia Chmaj <juliachmaj22@gmail.com>
2021-03-09 22:29:06 +0100

    test1

---
+++ clone.test.js
@@ -0,0 +1,15 @@
+const { test, expect } = require('@jest/globals')
+const clone = require('./clone')
+
+
+test('properly clones array', () => {
+    const array = [1,2,3]
+    expect(
+        clone(array)
+    ).toEqual(array)
+
+    expect(
+        clone(array)
+    ).not.toBe(array)
+
+})
---
+++ sum.js
@@ -0,0 +1,12 @@
+
+function sum(a,b) {
+    return a+b;
+
+
+}
+
+function helper() {
+    return "haha"
+}
+
+module.exports = sum
